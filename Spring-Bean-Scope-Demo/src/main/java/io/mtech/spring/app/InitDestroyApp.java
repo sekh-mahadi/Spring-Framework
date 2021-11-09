253abc9e3eb6271782dcb9c9bf643390b32df502
Sekh <sekh-mahadi>
2021-11-09 11:21:34 +0600

    Spring Bean Scope: Init and Destroy, postConstruct and preDestroy

---
+++ Spring-Bean-Scope-Demo/src/main/java/io/mtech/spring/app/InitDestroyApp.java
@@ -0,0 +1,28 @@
+package io.mtech.spring.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import io.mtech.spring.app.domain.Organization;
+
+public class InitDestroyApp {
+	
+	public static void main(String[] args) {
+// create the application context (container)
+		ApplicationContext ctx= new ClassPathXmlApplicationContext("beans-cp.xml");
+		//create the bean
+		Organization org=(Organization)ctx.getBean("myorg");
+		
+		
+		
+		//invoke the company via bean
+		//System.out.println(org.corporateSlogan());
+		
+		//print Organization details
+		System.out.println(org);
+		
+		System.out.println(org.corporateService());
+		
+		((ClassPathXmlApplicationContext)ctx).close();
+	}
+}
