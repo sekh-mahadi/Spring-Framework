2932f7e1ab7446de72e21caa16ba48ac48c268bd
Sekh <sekh-mahadi>
2021-12-22 09:05:49 +0600

    add some  users for in memory authentication

---
+++ spring-security-app/src/main/java/io/mtech/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package io.mtech.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//add our users for in memory authentication
+		UserBuilder users = User.withDefaultPasswordEncoder();
+
+		auth.inMemoryAuthentication().withUser(users.username("mahadi").password("test123").roles("EMPLOYEE"))
+				.withUser(users.username("arisha").password("test012").roles("MANAGER"))
+				.withUser(users.username("ariba").password("test012").roles("ADMIN"));
+	}
+
+}
