90fd931143e91b9f6b5af54313c7ded2e41a1efa
Sekh <sekh-mahadi>
2021-12-22 09:04:36 +0600

    Spring Security Initializer

---
+++ spring-security-app/src/main/java/io/mtech/springsecurity/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package io.mtech.springsecurity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
