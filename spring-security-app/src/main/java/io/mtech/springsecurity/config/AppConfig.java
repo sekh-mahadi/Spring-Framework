08c0a6074e3632b92b13c7fe021e2255e53844ef
Sekh <sekh-mahadi>
2021-12-28 10:30:17 +0600

    Completed: Add JDBC authentication

--- spring-security-app/src/main/java/io/mtech/springsecurity/config/AppConfig.java
+++ spring-security-app/src/main/java/io/mtech/springsecurity/config/AppConfig.java
@@ -1,16 +1,34 @@
 package io.mtech.springsecurity.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "io.mtech.springsecurity")
+@PropertySource("classpath:persistence-mysql.properties")
 public class AppConfig {
+
+	// set up variable to hold the properties
+	@Autowired
+	private Environment env;
+	// Set up a logger for diagnostics
+	Logger logger = Logger.getLogger(getClass().getName());
+
 // difine a bean for viewResolver
 	@Bean
 	public ViewResolver viewResolver() {
@@ -20,4 +38,48 @@ public class AppConfig {
 
 		return viewResolver;
 	}
+
+//Difine a bean for our security datasource
+	@Bean
+	public DataSource securityDataSource() {
+		// create connection pool
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+		// set the jdbc driver class
+		try {
+			securityDataSource.setDriverClass((env.getProperty("jdbc.driver")));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException();
+		}
+
+		// log the connection props
+		// just to make sure we are REALLY reading data from properties file
+		logger.info(">>> Jdbc.url= " + env.getProperty("jdbc.url"));
+		logger.info(">>> Jdbc.user= " + env.getProperty("jdbc.user"));
+
+		// set the database connection
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+		// set a connection pool props
+		securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+		return securityDataSource;
+
+	}
+
+	// need a helper method
+	// read environment property and convert to int
+
+	private int getIntProperty(String propName) {
+		String propVal = env.getProperty(propName);
+		// now convert to int
+		int intPropVal = Integer.parseInt(propVal);
+		return intPropVal;
+	}
+
 }
--- spring-security-app/src/main/java/io/mtech/springsecurity/config/SecurityConfig.java
+++ spring-security-app/src/main/java/io/mtech/springsecurity/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package io.mtech.springsecurity.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,40 +14,43 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	//add a reference to our security data source
+	@Autowired
+	private DataSource securityDataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       //add our users for in memory authentication
-		UserBuilder users = User.withDefaultPasswordEncoder();
-
-		auth.inMemoryAuthentication().withUser(users.username("mahadi").password("test123").roles("EMPLOYEE"))
-				.withUser(users.username("arisha").password("test012").roles("EMPLOYEE","MANAGER"))
-				.withUser(users.username("ariba").password("test012").roles("EMPLOYEE","ADMIN"));
+		// add our users for in memory authentication
+		/*
+		 * UserBuilder users = User.withDefaultPasswordEncoder();
+		 * 
+		 * auth.inMemoryAuthentication().withUser(users.username("mahadi").password(
+		 * "test123").roles("EMPLOYEE"))
+		 * .withUser(users.username("arisha").password("test012").roles("EMPLOYEE",
+		 * "MANAGER"))
+		 * .withUser(users.username("ariba").password("test012").roles("EMPLOYEE",
+		 * "ADMIN"));
+		 */
+		
+		//Use jdbc authentication ... oh yeah
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
 	}
 
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
-http.authorizeRequests()
+
+		http.authorizeRequests()
 //.anyRequest().authenticated()
-.antMatchers("/").hasRole("EMPLOYEE")
-.antMatchers("/leaders/**").hasRole("MANAGER")
-.antMatchers("/systems/**").hasRole("ADMIN")
-.and()
-.formLogin()
+				.antMatchers("/").hasRole("EMPLOYEE").antMatchers("/leaders/**").hasRole("MANAGER")
+				.antMatchers("/systems/**").hasRole("ADMIN").and().formLogin()
 //.loginPage("/loginPage") //normal login page
-.loginPage("/fancyLogin") //Bootstrap Login form
-.loginProcessingUrl("/authenticatedTheUser")
-.permitAll()
-.and()
-.logout()
-.permitAll()
-.and()
-.exceptionHandling()
-.accessDeniedPage("/access-denied");
+				.loginPage("/fancyLogin") // Bootstrap Login form
+				.loginProcessingUrl("/authenticatedTheUser").permitAll().and().logout().permitAll().and()
+				.exceptionHandling().accessDeniedPage("/access-denied");
 
 	}
 
-	
 }
