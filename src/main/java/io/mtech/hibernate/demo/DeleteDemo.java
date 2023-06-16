a362acf52e3de7c49ca5bdbb21cee9395c5ad9fb
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2023-06-16 12:05:58 +0600

    Hibernate: One to One uni-direction relationship->Delete operation.

--- src/main/java/io/mtech/hibernate/demo/DeleteDemo.java
+++ src/main/java/io/mtech/hibernate/demo/DeleteDemo.java
@@ -8,7 +8,7 @@ import io.mtech.hibernate.model.Instructor;
 import io.mtech.hibernate.model.InstructorDetail;
 import io.mtech.hibernate.model.Student;
 
-public class OneToOneDemo {
+public class DeleteDemo {
 
 	public static void main(String[] args) {
 		// Session Factory
@@ -20,32 +20,28 @@ public class OneToOneDemo {
 		// create session
 		Session session = sessionFactory.getCurrentSession();
 		try {
-			// Create a student Object
-			/*
-			 * Instructor tempInstructor = new Instructor("Sekh", "Mahadi",
-			 * "sekh@mtech.com"); InstructorDetail tempInstructorDetail = new
-			 * InstructorDetail("No Like No Comments", "LUV to Code");
-			 */
-			Instructor tempInstructor = new Instructor("Sekh", "Rupon", "rupon@mtech.com");
-			InstructorDetail tempInstructorDetail = new InstructorDetail("No Like comments", "LUV to Code");
 			
-			// Associate the objects
-			tempInstructor.setInstructorDetail(tempInstructorDetail);
 			// Start transaction
 			session.beginTransaction();
 
-			// Note: this will ALSO save the details object
-			// because of CascadeType.ALL
+			//get the primary key
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			// delete the Instructors
+			if (tempInstructor != null) {
+				System.out.println("Deleting: " + tempInstructor);
+				// Note: this will ALSO Delete associated details object
+				// because of CascadeType.ALL
 
-			// save the instructor object
-			System.out.println("Saving the Instructor...."+tempInstructor);
-			session.persist(tempInstructor);
+				session.remove(tempInstructor);
+			}
 			// commit
 			session.getTransaction().commit();
 			System.out.println("Done!");
-
 		} finally {
 			sessionFactory.close();
 		}
 	}
+
+
 }
