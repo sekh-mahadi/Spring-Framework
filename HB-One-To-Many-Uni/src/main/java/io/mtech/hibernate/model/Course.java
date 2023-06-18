ba28811caccce320f4e81037bea108bdc30ed8bc
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2023-06-18 16:02:12 +0600

    HB-One-To-Many-Uni:Completed reviews course.

--- HB-One-To-Many-Uni/src/main/java/io/mtech/hibernate/model/Course.java
+++ HB-One-To-Many-Uni/src/main/java/io/mtech/hibernate/model/Course.java
@@ -1,13 +1,18 @@
 package io.mtech.hibernate.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +30,10 @@ public class Course {
 	        CascadeType.DETACH, CascadeType.REFRESH})
 	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "course_id")
+	private List<Review> reviews;
 
 	public Course() {
 	}
@@ -55,6 +64,22 @@ public class Course {
 
 	public void setInstructor(Instructor instructor) {
 		this.instructor = instructor;
+	}
+
+	public List<Review> getReviews() {
+		return this.reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	// Add a convenience method
+	public void addReview(Review review) {
+		if (reviews == null) {
+			reviews = new ArrayList<>();
+		}
+		reviews.add(review);
 	}
 
 	@Override
---
+++ HB-One-To-Many-Uni/src/main/java/io/mtech/hibernate/model/Review.java
@@ -0,0 +1,43 @@
+package io.mtech.hibernate.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "review")
+public class Review {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	@Column(name = "comment")
+	private String comment;
+	
+	public Review() {
+	}
+
+	public Review(String comment) {
+		this.comment = comment;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return this.comment;
+	}
+
+	public void setComments(String comment) {
+		this.comment = comment;
+	}
+
+}
