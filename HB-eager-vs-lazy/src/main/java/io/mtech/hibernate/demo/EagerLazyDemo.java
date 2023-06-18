436973bfee320873b053d363109481bbf9dc267a
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2023-06-18 08:57:20 +0600

    Hibernate: Fetch Lazy->HQL Fetch JOIN..

--- HB-eager-vs-lazy/src/main/java/io/mtech/hibernate/demo/EagerLazyDemo.java
+++ HB-eager-vs-lazy/src/main/java/io/mtech/hibernate/demo/EagerLazyDemo.java
@@ -3,6 +3,7 @@ package io.mtech.hibernate.demo;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import io.mtech.hibernate.model.Course;
 import io.mtech.hibernate.model.Instructor;
@@ -22,19 +23,25 @@ public class EagerLazyDemo {
 		// create session
 		Session session = sessionFactory.getCurrentSession();
 		try {
-			// Start transaction
+			// Start trans action
 			session.beginTransaction();
 			// get the instructor from db
 			int theId = 1;
-			Instructor tempInstructor = session.get(Instructor.class, theId);
+			Query<Instructor> query = session.createQuery("select i from Instructor i "
+			        + "JOIN FETCH i.courses "
+			        + "where i.id=:theInstructorId", Instructor.class);
 
-			System.out.println("Mtech Instructor: "+tempInstructor);
+			query.setParameter("theInstructorId", theId);
 			
-			//get course for the instructor
-			System.out.println("Mtech: Courses: "+tempInstructor.getCourses());
-			
+			Instructor tempInstructor = query.getSingleResult();
+			System.out.println("Mtech Instructor: " + tempInstructor);
+
 			// commit
 			session.getTransaction().commit();
+			session.close();
+			
+			System.out.println("\nMtech Instructor: " + tempInstructor.getCourses());
+
 			System.out.println("Mtech: Done!");
 
 		} finally {
