07b659a3c29610dc186665cc22aadb074a60b4ae
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2023-06-18 09:30:40 +0600

    HB-One-To-Many-Uni:Init

---
+++ HB-One-To-Many-Uni/src/main/java/io/mtech/hibernate/model/InstructorDetail.java
@@ -0,0 +1,77 @@
+package io.mtech.hibernate.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "youtube_channel")
+	private String youtubeChannel;
+
+	@Column(name = "hobby")
+	private String hobby;
+
+	// add a new field for instructor(also getters/setters)
+	@OneToOne(mappedBy = "instructorDetail", cascade = {CascadeType.DETACH,
+	        CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.MERGE})
+	private Instructor instructor;
+
+	public InstructorDetail() {
+	}
+
+	public InstructorDetail(String youtubeChannel, String hobby) {
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getYoutubeChannel() {
+		return this.youtubeChannel;
+	}
+
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+	public String getHobby() {
+		return this.hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	public Instructor getInstructor() {
+		return this.instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + this.id + ", youtubeChannel="
+		        + this.youtubeChannel + ", hobby=" + this.hobby + "]";
+	}
+}
