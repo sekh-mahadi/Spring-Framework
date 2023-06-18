0593f303a6e81f2a6434e6bf26605ac393fd94c8
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2023-06-18 22:44:44 +0600

    HB-Many-To-Many: Delete specific Course.

---
+++ HB-Many-To-Many/src/main/java/io/mtech/hibernate/demo/DeleteBangla101Demo.java
@@ -0,0 +1,47 @@
+package io.mtech.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import io.mtech.hibernate.model.Course;
+import io.mtech.hibernate.model.Instructor;
+import io.mtech.hibernate.model.InstructorDetail;
+import io.mtech.hibernate.model.Review;
+import io.mtech.hibernate.model.Student;
+
+public class DeleteBangla101Demo {
+
+	public static void main(String[] args) {
+		// Session Factory
+		SessionFactory sessionFactory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class)
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		// create session
+		Session session = sessionFactory.getCurrentSession();
+		try {
+			// Start transaction
+			session.beginTransaction();
+			
+			int theId = 14;
+			Course tempCourse = session.get(Course.class, theId);
+			
+			System.out.println("\nLoaded Course: "+tempCourse);
+			//delete Course
+			session.remove(tempCourse);
+			
+			// commit
+			session.getTransaction().commit();
+			System.out.println("Mtech: Done!");
+
+		} finally {
+			session.close();
+			sessionFactory.close();
+		}
+	}
+}
