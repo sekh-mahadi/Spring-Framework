b06906cec4997c429fa037a1f50bc0d8710647e7
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2023-06-26 09:56:54 +0600

    AOP: Pointcut Expressions -> Wildcard.

--- Spring-AOP/src/main/java/io/mtech/aop/aspect/DemoLoggingAspect.java
+++ Spring-AOP/src/main/java/io/mtech/aop/aspect/DemoLoggingAspect.java
@@ -11,8 +11,10 @@ public class DemoLoggingAspect {
 	// this is where we add all our related advices for logging
 
 	// lets start with an @Before advice
-//	@Before("execution(public void io.mtech.aop.dao.AccountDAO.addAccount())")
-	@Before("execution(public void add*())")
+	// @Before("execution(public void
+	// io.mtech.aop.dao.AccountDAO.addAccount())")
+	// @Before("execution(void add*())")
+	@Before("execution(* add*())")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n========Executing @Before advice on addAccount()");
 	}
