627867cee853b7110e9465f996b69d9fa08cff50
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2023-06-28 11:15:06 +0600

    AOP: @after Returning. => Add afterReturningFindAccountAdvice() method to DemoLoggingAspect Class.

--- Spring-AOP-After-Returning/src/main/java/io/mtech/aop/aspect/DemoLoggingAspect.java
+++ Spring-AOP-After-Returning/src/main/java/io/mtech/aop/aspect/DemoLoggingAspect.java
@@ -1,6 +1,9 @@
 package io.mtech.aop.aspect;
 
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -13,6 +16,24 @@ import io.mtech.aop.entity.Account;
 @Component
 @Order(0)
 public class DemoLoggingAspect {
+
+	// add a new advice for @afterReturning on the findAccounts method
+
+	@AfterReturning(pointcut = "execution(* io.mtech.aop.dao.AccountDAO.findAccounts(..))", 
+			returning = "result")
+	public void afterReturingFidAccountAdvice(JoinPoint theJoinPoint, List<Account> result) {
+
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n=====>Executing @AfterReturning on method: "
+		        + method);
+
+		// print out the results of the method call
+		System.out.println("\n========> result is: " + result);
+		
+		
+	}
+
 	// this is where we add all our related advices for logging
 
 	@Before("io.mtech.aop.aspect.MtechAopExpressions.forDaoPackageNoGetterSetter()")
@@ -22,19 +43,19 @@ public class DemoLoggingAspect {
 		// Display the method signature arguments
 		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
 		System.out.println("Method: " + methodSig);
-		
+
 		// display method arguments
-		//get args 
+		// get args
 		Object[] args = theJoinPoint.getArgs();
-		
+
 		for (Object tempArg : args) {
 			System.out.println(tempArg);
 			if (tempArg instanceof Account) {
-				//downcast and print Account specific stuff
+				// downcast and print Account specific stuff
 				Account theAccount = (Account) tempArg;
-				
-				System.out.println("Account Name: "+theAccount.getName());
-				System.out.println("Account Level: "+theAccount.getLevel());
+
+				System.out.println("Account Name: " + theAccount.getName());
+				System.out.println("Account Level: " + theAccount.getLevel());
 			}
 		}
 	}
