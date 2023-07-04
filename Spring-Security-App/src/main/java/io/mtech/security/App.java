73aaf218eac9ad710af809e4f85ab26fc80e1f6c
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2023-07-04 10:37:50 +0600

    Project configured.

---
+++ Spring-Security-App/src/main/java/io/mtech/security/App.java
@@ -0,0 +1,13 @@
+package io.mtech.security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
